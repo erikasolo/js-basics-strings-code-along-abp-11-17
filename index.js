a950bc46ba16ce2773867df01c0862cf353025e3
Erika Solomon <mrserikasolomon@gmail.com>
2017-11-05 23:11:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const currentUser = 'Grace Hopper';
  
-...
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
  
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
