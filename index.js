5dd13394e1834a9d4d2788de02386370c9d302c4
Erika Solomon <mrserikasolomon@gmail.com>
2017-11-05 23:18:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 'Edsger Dijkstra'.length;
 //=> 15
- 
+
 'Edsger Dijkstra'[15];
 //=> undefined
- 
+
 'Edsger Dijkstra'[14];
 //=> "a"
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
